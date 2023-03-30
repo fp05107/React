bf4491fb4415712d0273c1cdb696c5a564e75d22
subham <andharalauri@gmail.com>
2023-03-30 20:26:49 +0530

    JSX

--- contextapi/src/App.js
+++ contextapi/src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
-import { createContext, useState } from 'react';
-import CompA from './components/CompA';
-
-const AppState = createContext();
+import React from 'react';
 
 function App() {
 
-  const [data,setData] = useState("Subham");
 
-  return (
-    <div className="App">
-      <AppState.Provider value={data}>
-        <CompA />
-      </AppState.Provider>
-    </div>
-  );
+  return React.createElement("div",{className:"App"},[
+    React.createElement("h1",{},"Hello World"),
+    React.createElement("h2",{},"Welcome To React")
+  ])
 }
 
 export default App;
-export {AppState}
+
+
+// export {AppState}
+// const [data,setData] = useState("Subham");
+// import { createContext, useState } from 'react';
+// import CompA from './components/CompA';
+
+// const AppState = createContext();
+// <AppState.Provider value={data}>
+// <CompA />
+// </AppState.Provider>
