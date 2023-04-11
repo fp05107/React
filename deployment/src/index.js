37690781a482c4e42ee1a29b0fd282b1398b9cd7
subham <andharalauri@gmail.com>
2023-04-11 09:39:38 +0530

    calculator fixed

--- deployment/src/index.js
+++ deployment/src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [display, setDisplay] = useState("");
+  const [expression, setExpression] = useState([]);
+
+  const handleClick = value => {
+    setDisplay(value);
+    setExpression([...expression, value]);
+  };
+
+  const handleResult = () => {
+    const result = expression
+      .join("")
+      .split(/(\D)/g)
+      .map(value => (value.match(/\d/g) ? parseInt(value, 0) : value))
+      .reduce((acc, value, index, array) => {
+        switch (value) {
+          case "+":
+            return (acc = acc + array[index + 1]);
+          case "-":
+            return (acc = acc - array[index + 1]);
+          case "x":
+            return (acc = acc * array[index + 1]);
+          case "÷":
+            return (acc = acc / array[index + 1]);
+          default:
+            return acc;
+        }
+      });
+    setDisplay(result);
+    setExpression("");
+  };
+
+  return (
+    <div className="App">
+      <h3 className="display">{display}</h3>
+
+      <span className="expression">{expression}</span>
+
+      <section className="panel">
+        <section className="numbers">
+          <button onClick={() => handleClick(7)}>7</button>
+          <button onClick={() => handleClick(8)}>8</button>
+          <button onClick={() => handleClick(9)}>9</button>
+
+          <button onClick={() => handleClick(4)}>4</button>
+          <button onClick={() => handleClick(5)}>5</button>
+          <button onClick={() => handleClick(6)}>6</button>
+
+          <button onClick={() => handleClick(1)}>1</button>
+          <button onClick={() => handleClick(2)}>2</button>
+          <button onClick={() => handleClick(3)}>3</button>
+
+          <button onClick={() => handleClick(0)}>0</button>
+        </section>
+
+        <section className="operators">
+          <button onClick={() => handleClick("÷")}>÷</button>
+          <button onClick={() => handleClick("x")}>x</button>
+          <button onClick={() => handleClick("-")}>-</button>
+          <button onClick={() => handleClick("+")}>+</button>
+          <button onClick={() => handleResult()}>=</button>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Counter />, rootElement);
